fd756d1e39948013568a67c9497a1a15b4fb66be
Lawrence Yu <lawrenceyu1@gmail.com>
2018-01-11 13:41:30 -0800

    Add roles resource

--- lib/frederick_api/v2/role.rb
+++ lib/frederick_api/v2/role.rb
@@ -3,8 +3,8 @@
 module FrederickAPI
   module V2
     # /v2/users
-    class User < Resource
-      has_many :roles, class_name: 'FrederickAPI::V2::Role'
+    class Role < Resource
+      has_many :users
     end
   end
 end
