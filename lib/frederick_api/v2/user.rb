6951625cb8519d2dc6231a0ce3b0997e7941473c
Lawrence Yu <lawrenceyu1@gmail.com>
2018-05-29 12:31:22 -0700

    Add permitted location to user resource

--- lib/frederick_api/v2/user.rb
+++ lib/frederick_api/v2/user.rb
@@ -5,6 +5,7 @@ module FrederickAPI
     # /v2/users
     class User < Resource
       has_many :roles, class_name: 'FrederickAPI::V2::Role'
+      has_many :permitted_locations, class_name: 'FrederickAPI::V2::Location'
     end
   end
 end
