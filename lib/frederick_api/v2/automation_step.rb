be96caaa08665965d31748518a6100af4f64d3ec
ccangialose <claire.cangialose@mindbodyonline.com>
2020-12-17 18:22:19 -0800

    Add previous automation step

--- lib/frederick_api/v2/automation_step.rb
+++ lib/frederick_api/v2/automation_step.rb
@@ -6,6 +6,7 @@ module FrederickAPI
     class AutomationStep < Resource
       belongs_to :location
       has_one :automation
+      has_one :previous_automation_step, class_name: 'FrederickAPI::V2::AutomationStep'
 
       self.read_only_attributes += %i[location_id]
     end
