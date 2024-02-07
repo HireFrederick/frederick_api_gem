18d90ddaf5f24623cac1fe03f131c9275144154f
Neetesh-Mindbody <neetesh.mittal@mindbodyonline.com>
2024-02-07 10:33:13 +0530

    raising next link not found error only in valid cases

--- lib/frederick_api/v2/helpers/paginator.rb
+++ lib/frederick_api/v2/helpers/paginator.rb
@@ -15,13 +15,17 @@ module FrederickAPI
           results = self.result_set.to_a
           first_resource = self.result_set.first
 
-          (total_pages - current_page).times do
+          remaining_pages = total_pages - current_page
+
+          remaining_pages.times do |page|
             first_resource.class.with_headers(first_resource.custom_headers) do
               current_result_set = next_result_set(current_result_set)
-              raise 'next link not found' unless current_result_set
+              break if (page + 1 >= remaining_pages) && current_result_set.nil?
+              raise 'next link not found' if current_result_set.nil?
               results.push(*current_result_set.to_a)
             end
           end
+
           results
         end
 
@@ -53,10 +57,11 @@ module FrederickAPI
           def next_result_set(current_result_set)
             if current_result_set
               response = current_result_set.pages.next
-              response.present? ? response : current_result_set.pages.next
             else
-              self.result_set.pages.next
+              response = self.result_set.pages.next
             end
+
+            response.present? ? response : nil
           end
       end
     end
