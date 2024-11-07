44e5c8e9c81157c42d21b7f580fc51cab77ae641
Shubham Surana <Shubham.surana@mindbodyonline.com>
2024-11-07 11:38:30 +0530

    log the number of attempts (#27)

--- lib/frederick_api/v2/helpers/retrier.rb
+++ lib/frederick_api/v2/helpers/retrier.rb
@@ -8,12 +8,19 @@ module FrederickAPI
         def retry_block(n)
           attempts = 1
           begin
+            nr_log_attempts(attempts)
             yield
           rescue
             attempts += 1
             attempts <= n ? retry : raise
             sleep(attempts * 5)
           end
+        end
+
+        def nr_log_attempts(attempts)
+          NewRelic::Agent.record_metric('FrolodexPageFetchAttempt', attempts)
+        rescue
+          nil
         end
       end
     end
